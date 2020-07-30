584b32fb12589f287d488d57164e0a718383ffb2
Matt Lewis <matt@mplewis.com>
2020-07-30 10:22:05 -0600

    basic fixes, cant rm rf tests

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,13 @@
 FROM lambci/lambda:build-python3.6
 
+WORKDIR /
+RUN pip install virtualenv
+RUN virtualenv -p /var/lang/bin/python --always-copy lambda_build
+
 RUN mkdir /app
-COPY docker-lambda-with-virtual.sh /app/.
-COPY numpy_pickle_utils.py /app/.
+COPY docker-lambda-with-virtual.sh /app
+COPY numpy_pickle_utils.py /app
 COPY requirements.txt /app
 
 # Set the default directory where CMD will execute
-WORKDIR /
-CMD ["/bin/bash"] 
-
+CMD ["/bin/bash"]
--- docker-lambda-with-virtual.sh
+++ docker-lambda-with-virtual.sh
@@ -1,49 +1,41 @@
 #!/bin/bash
+set -euxo pipefail
+
 do_pip () {
-		pip install --upgrade pip wheel		
-		pip install -r /app/requirements.txt
-	}
+    pip install --upgrade pip wheel
+    pip install -r /app/requirements.txt
+}
 
 strip_virtualenv () {
-		echo "original size $(du -sh $VIRTUAL_ENV | cut -f1)"
-		find $VIRTUAL_ENV/lib/python3.6/site-packages/ -name "tests*"| xargs rm -r
-		find $VIRTUAL_ENV/lib/python3.6/site-packages/ -name "dataset*" | xargs rm -r
+    echo "original size $(du -sh $VIRTUAL_ENV | cut -f1)"
+    whoami
+    find $VIRTUAL_ENV/lib/python3.6/site-packages/ -name "tests*" | xargs ls -la
+    find $VIRTUAL_ENV/lib/python3.6/site-packages/ -name "tests*" | xargs rm -r
+    find $VIRTUAL_ENV/lib/python3.6/site-packages/ -name "dataset*" | xargs rm -r
 
-		# Can't remove tests files from pandas
-		pushd $VIRTUAL_ENV/lib/python3.6/site-packages/scipy/.libs && \
-		      rm *; ln ../../numpy/.libs/* . && \
-              rm -rf /root/.cache  ; popd
+    # Can't remove tests files from pandas
+    pushd $VIRTUAL_ENV/lib/python3.6/site-packages/scipy/.libs && \
+            rm *; ln ../../numpy/.libs/* . && \
+            rm -rf /root/.cache  ; popd
 
-		find $VIRTUAL_ENV/lib/python3.6/site-packages/ -name "*.txt" | xargs rm -r
-		find $VIRTUAL_ENV/lib/python3.6/site-packages/ -name "*.so" | grep -v ufuncs | grep -v fblas | grep -v flapack | \
-                                                                    grep -v cython_blas | grep -v cython_lapack | grep -v ellip_harm | \
-                                                                    grep -v odepack | grep -v quadpack | grep -v vode | grep -v lsoda | \
-                                                                    grep -v iterative | grep -v superlu | grep -v arpack | grep -v trlib | \
-                                                                    grep -v lbfgs | grep -v qhull | xargs strip
-		find $VIRTUAL_ENV/lib/python3.6/site-packages/ -name "*.pyc" -delete
-		find $VIRTUAL_ENV/lib/python3.6/site-packages/ -type d -empty -delete
-		echo "current size $(du -sh $VIRTUAL_ENV | cut -f1)"
-	    pushd $VIRTUAL_ENV/lib/python3.6/site-packages/ && zip -r -9 -q /outputs/lambda.zip scipy numpy sklearn pandas pytz lightgbm ; popd
-	}
+    find $VIRTUAL_ENV/lib/python3.6/site-packages/ -name "*.txt" | xargs rm -r
+    find $VIRTUAL_ENV/lib/python3.6/site-packages/ -name "*.so" | grep -v ufuncs | grep -v fblas | grep -v flapack | \
+                                                                grep -v cython_blas | grep -v cython_lapack | grep -v ellip_harm | \
+                                                                grep -v odepack | grep -v quadpack | grep -v vode | grep -v lsoda | \
+                                                                grep -v iterative | grep -v superlu | grep -v arpack | grep -v trlib | \
+                                                                grep -v lbfgs | grep -v qhull | xargs strip
+    find $VIRTUAL_ENV/lib/python3.6/site-packages/ -name "*.pyc" -delete
+    find $VIRTUAL_ENV/lib/python3.6/site-packages/ -type d -empty -delete
+    echo "current size $(du -sh $VIRTUAL_ENV | cut -f1)"
+    pushd $VIRTUAL_ENV/lib/python3.6/site-packages/ && zip -r -9 -q /outputs/lambda.zip scipy numpy sklearn pandas pytz lightgbm ; popd
+}
 
 main () {
-        apt-get update
-        apt-get install zip unzip
-		pip install virtualenv
-		virtualenv -p /usr/local/bin/python \
-			        --always-copy \
-			        --no-site-packages \
-			        lambda_build
+    source lambda_build/bin/activate
 
-		source lambda_build/bin/activate
-    
-    	do_pip
-
-		
-    	strip_virtualenv		
-
-    	pip list
+    do_pip
+    strip_virtualenv
+    pip list
 }
 
 main
-
